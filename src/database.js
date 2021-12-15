e6d014a7b84017f39f4a5b0c8113371973732d5c
Jere Kankaanranta <jere@kankje.com>
2021-12-15 17:44:03 +0200

    Retry database connection

--- src/database.js
+++ src/database.js
@@ -2,28 +2,36 @@ const { Client } = require('pg')
 
 let client = null
 
-module.exports = {
-  async connectToDatabase() {
-    try {
-      client = new Client({
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-      })
-      await client.connect()
-      await client.query('CREATE TABLE IF NOT EXISTS counter (value int)')
+const RETRY_DELAY_SECONDS = 30
 
-      const result = await client.query('SELECT * FROM counter')
+async function connectToDatabase(retries = 0) {
+  try {
+    client = new Client({
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    })
+    await client.connect()
+    await client.query('CREATE TABLE IF NOT EXISTS counter (value int)')
 
-      if (result.rows.length === 0) {
-        await client.query('INSERT INTO counter VALUES (0)')
-      }
-    } catch (e) {
-      console.warn('Database connection failed, the `/increment`-endpoint will not work:', e)
+    const result = await client.query('SELECT * FROM counter')
+
+    if (result.rows.length === 0) {
+      await client.query('INSERT INTO counter VALUES (0)')
     }
-  },
+
+    console.info('Database connection established')
+  } catch (e) {
+    console.warn('Database connection failed, the `/increment`-endpoint will not work:', e)
+    console.info(`Retrying database connection in ${RETRY_DELAY_SECONDS}s`)
+    setTimeout(() => connectToDatabase(retries + 1), RETRY_DELAY_SECONDS * 1000)
+  }
+}
+
+module.exports = {
+  connectToDatabase,
 
   getClient() {
     if (!client) {
